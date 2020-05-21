4bc63f047257f0c0887968876f563b303718129c
Adam Jinks <adam@echo9.net>
2020-05-21 02:29:08 -0700

    route53 public
    tf 12

---
+++ route53-public/main.tf
@@ -0,0 +1,19 @@
+locals {
+  description = Public zone for var.name
+  managed_by  = "Terraform 0.12"
+}
+
+resource "aws_route53_zone" "this" {
+  name              = var.name
+  comment           = local.description
+  delegation_set_id = var.delegation_set_id
+  force_destroy     = var.force_destroy
+
+  tags = {
+    Name          = var.name
+    ProductDomain = var.product_domain
+    Environment   = var.environment
+    Description   = local.description
+    ManagedBy     = local.managed_by
+  }
+}
